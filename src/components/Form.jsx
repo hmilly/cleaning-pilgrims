f7cb7e489ea98a4912741330f7dc87aa3f528889
Holly <milstowers@gmail.com>
2023-02-21 04:48:11 +0000

    +draft form data, made nav responsive, upd styles

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,9 +3,35 @@ const Form = () => {
     <form>
       <h3>Get a Cleaning Estimate</h3>
       <div>
-        <label htmlFor="">
-          <input type="text"/>
-        </label>
+        {/* <TextField
+          helperText=" "
+          id="name"
+          label="Name"
+        /> */}
+
+        {/* <TextField
+          helperText=" "
+          id="email"
+          label="Email"
+        /> */}
+
+        {/* <TextField
+          helperText=" "
+          id="frequency"
+          label="Frequency of cleaning"
+        /> */}
+
+        {/* <TextField
+          helperText=" "
+          id="footage"
+          label="Square footage to be cleaned"
+        /> */}
+
+        {/* <TextareaAutosize 
+          helperText=" "
+          id="text"
+          placeholder='Tell us more about your cleaning needs so we can give you a more accurate estimate. How is the space used? Does it have any special cleaning needs?" data-aid="Tell us more about your cleaning needs so we can give you a more accurate estimate. How is the space used? Does it have any special cleaning needs?'
+        /> */}
       </div>
     </form>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,52 @@
 import { Link } from "react-router-dom";
+import { SlMenu } from "react-icons/sl";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const [windowSize, setWindowSize] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleWindowResize = () => setWindowSize(window.innerWidth);
+    window.addEventListener("resize", handleWindowResize);
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  });
+
   return (
-    <header className="header">
+    <header>
       <nav>
-        <ul>
-          <li>
-            <Link to="/">HOME</Link>
-          </li>
-          <li>
-            <Link to="/contact">CONTACT</Link>
-          </li>
-          <li>
-            <Link id="lg-txt" to="/">
-              KENT CLEANING PILGRIMS LTD.
-            </Link>
-          </li>
-          <li>
-            <Link to="/about">ABOUT</Link>
-          </li>
-        </ul>
+        {windowSize >= 769 ? (
+          <ul>
+            <li>
+              <Link to="/">HOME</Link>
+            </li>
+            <li>
+              <Link to="/contact">CONTACT</Link>
+            </li>
+            <li id="lg-li">
+              <Link id="lg-txt" to="/">
+                KENT CLEANING PILGRIMS LTD.
+              </Link>
+            </li>
+            <li>
+              <Link to="/about">ABOUT</Link>
+            </li>
+          </ul>
+        ) : (
+          <ul>
+            <li>
+              <button>
+                <SlMenu className="icon" />
+              </button>
+            </li>
+            <li id="lg-li">
+              <Link id="lg-txt" to="/">
+                KENT CLEANING PILGRIMS LTD.
+              </Link>
+            </li>
+          </ul>
+        )}
       </nav>
     </header>
   );
