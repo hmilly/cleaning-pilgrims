c1564b18d0f42f53d719cb151ae0d6b9d1c9abf4
Holly <milstowers@gmail.com>
2023-02-22 04:52:08 +0000

    adding form data and styles

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,38 +1,78 @@
+import { useState } from "react";
+
 const Form = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    frequency: "",
+    sqFoot: 0,
+    text: "",
+  });
+
+  const addFormData = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmit = () => {
+    console.log(formData)
+  }
+
   return (
     <form>
       <h3>Get a Cleaning Estimate</h3>
-      <div>
-        {/* <TextField
-          helperText=" "
+      <div className='inputbox'>
+        <input
+          type="text"
+          required
           id="name"
-          label="Name"
-        /> */}
-
-        {/* <TextField
-          helperText=" "
-          id="email"
-          label="Email"
-        /> */}
-
-        {/* <TextField
-          helperText=" "
-          id="frequency"
-          label="Frequency of cleaning"
-        /> */}
-
-        {/* <TextField
-          helperText=" "
-          id="footage"
-          label="Square footage to be cleaned"
-        /> */}
-
-        {/* <TextareaAutosize 
-          helperText=" "
-          id="text"
-          placeholder='Tell us more about your cleaning needs so we can give you a more accurate estimate. How is the space used? Does it have any special cleaning needs?" data-aid="Tell us more about your cleaning needs so we can give you a more accurate estimate. How is the space used? Does it have any special cleaning needs?'
-        /> */}
+          value={formData.name}
+          onChange={addFormData}
+        />
+        <span>Name</span>
       </div>
+      <div className='inputbox'>
+        <input
+          type="email"
+          required
+          id="email"
+          value={formData.email}
+          onChange={addFormData}
+        />
+        <span>Email</span>
+      </div>
+      <div className='inputbox'>
+        <input
+          type="text"
+          required
+          id="frequency"
+          value={formData.frequency}
+          onChange={addFormData}
+        />
+        <span>Frequency of cleaning</span>
+      </div>
+      <div className='inputbox'>
+        <input
+          type="number"
+          required
+          id="sqFoot"
+          value={formData.sqFoot}
+          onChange={addFormData}
+        />
+        <span>Sq foot to be cleaned</span>
+      </div>
+
+      <textarea
+        type="text"
+        required
+        id="text"
+        value={formData.text}
+        onChange={addFormData}
+        placeholder='Tell us more about your cleaning needs so we can give you a more accurate estimate. How is the space used? Does it have any special cleaning needs?" data-aid="Tell us more about your cleaning needs so we can give you a more accurate estimate. How is the space used? Does it have any special cleaning needs?'
+      />
+      <button type="submit" onClick={onSubmit}>Send</button>
     </form>
   );
 };
